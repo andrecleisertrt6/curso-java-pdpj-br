90f84c43d57dd0f9f0d41ec6b06777009d0c9f7b
Andre Cleiser <andre.cleiser@polarising.com>
2024-03-09 09:20:25 +0000

    Primeiro commit

---
+++ src/main/java/br/com/curso/pdpj/CursoJavaAvancadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.curso.pdpj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaAvancadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaAvancadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/curso/pdpj/CursoJavaAvancadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.curso.pdpj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaAvancadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
